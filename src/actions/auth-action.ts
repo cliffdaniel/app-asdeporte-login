98909dccd7d66e47a1a3244ec2b10cf2bc2116f4
Daniel Arenas <jdaniel.arenas@gmail.com>
2024-09-18 21:39:35 -0500

    chore: move auth.config

--- src/actions/auth-action.ts
+++ src/actions/auth-action.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { signIn } from '../../auth';
+import { signIn } from '@/auth';
 import { loginSchema } from '@/lib/zod';
 import { AuthError } from 'next-auth';
 import { z } from 'zod';
